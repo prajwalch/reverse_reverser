f96c93e2769cedbd4d6dca985a11591334b36fb1
PrajwalCH <prajjwal2058@gmail.com>
2021-08-16 19:51:08 +0545

    new main.c file

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Author      : Prajwal Chapagain <prajjwal2058@gmail.com>
+ * Date        : Sunday Aug 15, 2021 17:06:08 NPT
+ * License     : MIT
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!!\n");
+    return 0;
+}
+
